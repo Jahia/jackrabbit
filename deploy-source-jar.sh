bfe0fee724470b62707a86987515139aa8cda8a1
Thomas Draier <tdraier@jahia.com>
2010-02-02 10:43:33 +0000

    package and deploy sources jar script
    git-svn-id: https://devtools.jahia.com/svn/jackrabbit/trunk@3 25beda46-67d2-432a-828b-91af10607620

--- deploy-source-jar.sh
+++ deploy-source-jar.sh
@@ -1,1 +1,3 @@
-mvn deploy:deploy-file -DgroupId=org.apache.jackrabbit -Durl=scpexe://maven.jahia.org/var/www/vhosts/maven.jahia.org/html/maven2 -DartifactId=$1 -Dversion=$2 -Dclassifier=sources -Dpackaging=jar -DrepositoryId=jahiaRepository -Dfile=./$1/target/$1-$2-sources.jar
+cd $1
+mvn source:jar
+mvn deploy:deploy-file -DgroupId=org.apache.jackrabbit -Durl=scpexe://maven.jahia.org/var/www/vhosts/maven.jahia.org/html/maven2 -DartifactId=$1 -Dversion=$2 -Dclassifier=sources -Dpackaging=jar -DrepositoryId=jahiaRepository -Dfile=./target/$1-$2-sources.jar
