559802d5083a44e082665af4f602e371f39c8d8d
Serge Huber <shuber@jahia.com>
2010-10-25 08:38:52 +0000

    New trunk where we will import the current Jackrabbit 2.2 trunk
    git-svn-id: https://devtools.jahia.com/svn/jackrabbit/trunk@21 25beda46-67d2-432a-828b-91af10607620

--- deploy-source-jar.sh
+++
@@ -1,3 +0,0 @@
-cd $1
-mvn source:jar
-mvn deploy:deploy-file -DgroupId=org.apache.jackrabbit -Durl=scpexe://maven.jahia.org/var/www/vhosts/maven.jahia.org/html/maven2 -DartifactId=$1 -Dversion=$2 -Dclassifier=sources -Dpackaging=jar -DrepositoryId=jahiaRepository -Dfile=./target/$1-$2-sources.jar
