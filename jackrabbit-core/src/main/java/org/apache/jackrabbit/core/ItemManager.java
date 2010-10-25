c9811ab44ccea4ec879f28e07622baa6d1c8c241
Serge Huber <shuber@jahia.com>
2010-10-25 13:56:09 +0000

    rev5 evict node from cache on shareable node modification ( JCR-2494 )
    git-svn-id: https://devtools.jahia.com/svn/jackrabbit/trunk@27 25beda46-67d2-432a-828b-91af10607620

--- jackrabbit-core/src/main/java/org/apache/jackrabbit/core/ItemManager.java
+++ jackrabbit-core/src/main/java/org/apache/jackrabbit/core/ItemManager.java
@@ -1043,17 +1043,15 @@ public class ItemManager implements ItemStateListener {
         ItemData data = retrieveItem(modified.getId());
         if (data != null && data.getState() == modified) {
             data.setStatus(ItemImpl.STATUS_MODIFIED);
-            /*
             if (modified.isNode()) {
                 NodeState state = (NodeState) modified;
                 if (state.isShareable()) {
-                    //evictItem(modified.getId());
-                    NodeData nodeData = (NodeData) data;
-                    NodeData shareSibling = new NodeData(nodeData, state.getParentId());
-                    shareableNodesCache.cache(shareSibling);
+                    evictItems(modified.getId());
+//                    NodeData nodeData = (NodeData) data;
+//                    NodeData shareSibling = new NodeData(nodeData, state.getParentId());
+//                    shareableNodesCache.cache(shareSibling);
                 }
             }
-            */
         }
     }
 
