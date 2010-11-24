0e9aeac46aacb76ae8f2e0accaefbb6c90f113ad
Serge Huber <shuber@jahia.com>
2010-11-24 09:57:19 +0000

    Skip tests when deploying JARs
    git-svn-id: https://devtools.jahia.com/svn/jackrabbit/trunk@44 25beda46-67d2-432a-828b-91af10607620

--- deploy_jars.sh
+++ deploy_jars.sh
@@ -1,1 +1,1 @@
-mvn deploy -DaltDeploymentRepository=jahiaRepository::default::scpexe://maven.jahia.org/var/www/vhosts/maven.jahia.org/html/maven2
+mvn deploy -DaltDeploymentRepository=jahiaRepository::default::scpexe://maven.jahia.org/var/www/vhosts/maven.jahia.org/html/maven2 -Dmaven.test.skip=true
