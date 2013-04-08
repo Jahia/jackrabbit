707d58f2205548b6e771c8bbd6272c6bd8a3e22d
Sergiy Shyrkov <sshyrkov@jahia.com>
2013-04-08 07:36:17 +0000

    Switched version to a SNAPSHOT (2.4.2-patch10-SNAPSHOT)
    git-svn-id: https://devtools.jahia.com/svn/jackrabbit/trunk@113 25beda46-67d2-432a-828b-91af10607620

--- deploy_sources_jar.sh
+++
@@ -1,3 +0,0 @@
-cd $1
-mvn source:jar
-mvn deploy:deploy-file -DgroupId=org.apache.jackrabbit -Durl=https://devtools.jahia.com/nexus/content/repositories/thirdparty-releases -DartifactId=$1 -Dversion=$2 -Dclassifier=sources -Dpackaging=jar -DrepositoryId=thirdparty-releases -Dfile=./target/$1-$2-sources.jar
