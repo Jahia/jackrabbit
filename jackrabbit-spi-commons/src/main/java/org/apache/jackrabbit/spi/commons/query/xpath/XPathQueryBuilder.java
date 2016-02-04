6e98b6e17d0e7f528f5c897f8ce5451b8016d4a2
Benjamin Papež <bpapez@jahia.com>
2016-02-04 15:56:08 +0000

    QA-3519: backport Jackrabbit rev. 1436914 and 1436916
    git-svn-id: https://devtools.jahia.com/svn/jackrabbit/trunk@244 25beda46-67d2-432a-828b-91af10607620

--- jackrabbit-spi-commons/src/main/java/org/apache/jackrabbit/spi/commons/query/xpath/XPathQueryBuilder.java
+++ jackrabbit-spi-commons/src/main/java/org/apache/jackrabbit/spi/commons/query/xpath/XPathQueryBuilder.java
@@ -83,6 +83,11 @@ public class XPathQueryBuilder implements XPathVisitor, XPathTreeConstants {
      * Name for 'fn:upper-case'
      */
     static final Name FN_UPPER_CASE = NAME_FACTORY.create(NS_FN_URI, "upper-case");
+    
+    /**
+     * Name for 'rep:normalize'
+     */
+    static final Name REP_NORMALIZE = NAME_FACTORY.create(Name.NS_REP_URI, "normalize");    
 
     /**
      * Name for 'not' as defined in XPath 1.0 (no prefix)
@@ -1006,6 +1011,17 @@ public class XPathQueryBuilder implements XPathVisitor, XPathTreeConstants {
                 } else {
                     exceptions.add(new InvalidQueryException("Unsupported location for fn:upper-case()"));
                 }
+            } else if (REP_NORMALIZE.equals(funName)) {
+                if (node.jjtGetNumChildren() == 2) {
+                    if (queryNode.getType() == QueryNode.TYPE_ORDER) {
+                        ((OrderQueryNode) queryNode).setFunction(REP_NORMALIZE.getLocalName());
+                        node.childrenAccept(this, queryNode);
+                    } else {
+                        exceptions.add(new InvalidQueryException("Unsupported location for rep:normalize()"));
+                    }
+                } else {
+                    exceptions.add(new InvalidQueryException("Wrong number of argument for rep:normalize()"));
+                }                
             } else if (REP_SIMILAR.equals(funName)) {
                 if (node.jjtGetNumChildren() == 3) {
                     if (queryNode instanceof NAryQueryNode) {
